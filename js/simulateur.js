ca56cd649d1bb0f231b98ebca5339cb07e195ef5
Matteo Bruccoleri <matteobruccolerii@gmail.com>
2024-10-09 13:52:12 +0200

    maj

--- js/simulateur.js
+++ js/simulateur.js
@@ -1,5 +1,6 @@
 let graphiqueEcos;
 let sortedData = [];
+let isButtonClicked = false; // Variable pour suivre si le bouton a été cliqué
 
 // Liste des matières
 const matieres = [
@@ -77,22 +78,31 @@ function initializeSortedData(notes, classements) {
 // Fonction pour mettre à jour la valeur affichée du slider
 function updateSliderValue(spanId, value) {
   document.getElementById(spanId).innerText = value;
-  updateMarker(parseFloat(document.getElementById('noteEDN').value), parseFloat(document.getElementById('noteECOS').value));
 }
 
 // Fonction pour créer un graphique avec Chart.js
 function createChart(matiere, notes, classements, limite) {
-  
   initializeSortedData(notes, classements);
-
   const sortedNotes = sortedData.map((d) => d.note);
 
-  // Calculer la régression polynomiale de degré 3
-  const result = regression.polynomial(
-    sortedData.map((d) => [d.note, d.classement]),
-    { order: 3 }
-  );
-  const regressionLine = result.points;
+  // Obtenir la note minimum et maximum pour la matière sélectionnée
+  const minNote = Math.min(...sortedNotes);
+  const maxNote = Math.max(...sortedNotes);
+
+// Calculer la régression polynomiale de degré 3
+const result = regression.polynomial(
+  sortedData.map((d) => [d.note, d.classement]),
+  { order: 3 }
+);
+
+// Ajuster manuellement la courbe aux extrémités pour passer par les points réels
+let regressionLine = result.points;
+
+// Forcer le premier point de la courbe de régression à correspondre exactement à la note et au classement minimum
+regressionLine[0] = [sortedData[0].note, sortedData[0].classement];
+
+// Forcer le dernier point de la courbe de régression à correspondre exactement à la note et au classement maximum
+regressionLine[regressionLine.length - 1] = [sortedData[sortedData.length - 1].note, sortedData[sortedData.length - 1].classement];
 
   // Créer un nouvel élément canvas pour chaque graphique
   const canvas = document.createElement("canvas");
@@ -106,24 +116,27 @@ function createChart(matiere, notes, classements, limite) {
   graphiqueEcos = new Chart(canvas, {
     type: "line",
     data: {
-      labels: sortedNotes, // Les notes sur l'axe X, triées
+      labels: sortedNotes,
       datasets: [
         {
           label: `Classement pour ${matiere}`,
-          data: sortedData.map((d) => [d.note, d.classement]), 
-          borderColor: "transparent",
-          tension: 0,
-          showLine: false,
+          data: sortedData.map((d) => ({ x: d.note, y: d.classement })), // Utilisation de {x, y} pour une meilleure flexibilité
+          borderColor: "rgba(0, 0, 0, 0.8)",
+          backgroundColor: "transparent",
+          tension: 0.4,
+          fill: false,
           pointBackgroundColor: "black",
-          pointRadius: 2
+          pointRadius: 2,
+          showLine: false,
         },
         {
           label: `Rang limite pour ${matiere}`,
-          data: Array(sortedNotes.length).fill(limite), // La limite à afficher comme ligne
+          data: Array(sortedNotes.length).fill(limite),
           fill: true,
           borderColor: "#48DE5A",
-          backgroundColor: "rgba(72, 222, 90, 0.10)",
-          pointRadius: 0, // Pas de points
+          backgroundColor: "rgba(72, 222, 90, 0.20)",
+          pointRadius: 0,
+          borderWidth: 0.5,
         },
         {
           label: "Régression lisse",
@@ -132,34 +145,39 @@ function createChart(matiere, notes, classements, limite) {
           pointRadius: 0,
           borderWidth: 2,
           showLine: true,
-          tension: 0.4,
+          tension: 0.3,
           fill: false,
-          backgroundColor: "rgba(0, 115, 250, 0.25)",
         },
       ],
     },
     options: {
       scales: {
+        x: {
+          type: 'linear',
+          min: minNote,
+          max: maxNote,
+          title: {
+            display: true,
+            text: "Note Totale",
+          },
+          ticks: {
+            align: 'center', // Positionne le texte de l'étiquette horizontalement au centre de chaque tick
+            crossAlign: 'center', // Aligne le texte de l'étiquette verticalement au centre
+            padding: 5, // Ajuste l'espace autour du texte
+          },
+        },
         y: {
-          reverse: true, // Inverser l'axe des Y (de 10000 à 0)
-          min: 0, // Minimum sur l'axe Y
-          max: 10500, // Maximum sur l'axe Y
+          reverse: true,
+          min: -500,
+          max: 10500,
           title: {
             display: true,
             text: "Classement",
           },
-        },
-        x: {
-          type: 'linear',
-          reverse: false, // Abscisse croissante, donc on désactive l'inversion ici
-          min: 9.5,
-          max: 17,
           ticks: {
-            stepSize: 1, 
-          },
-          title: {
-            display: true,
-            text: "Note Totale",
+            align: 'start', // Positionne le texte de l'étiquette horizontalement au début
+            crossAlign: 'center', // Aligne le texte de l'étiquette verticalement au centre
+            padding: 10, // Ajuste l'espace autour du texte pour le séparer de l'axe
           },
         },
       },
@@ -167,28 +185,50 @@ function createChart(matiere, notes, classements, limite) {
   });
 }
 
+
+// Fonction pour mettre à jour le marqueur du classement estimé
 function updateMarker(noteEDN, noteECOS) {
-  const noteTotale = Math.round((2 / 3 * noteEDN + 1 / 3 * noteECOS) * 100) / 100; 
+  if (!isButtonClicked) return; // Ne pas afficher le marqueur si le bouton n'a pas été cliqué
 
-  // Trouver le point le plus proche dans les données
-  const pointGraphique = sortedData.reduce((prev, curr) => {
-    return (Math.abs(curr.note - noteTotale) < Math.abs(prev.note - noteTotale) ? curr : prev);
-  });
+  const noteTotale = Math.round((2 / 3 * noteEDN + 1 / 3 * noteECOS) * 100) / 100;
 
+  // Vérifier si la noteTotale est en dehors de la plage des données
+  const minNote = Math.min(...sortedData.map(d => d.note));
+  const maxNote = Math.max(...sortedData.map(d => d.note));
+
+  let classementEstime;
+  if (noteTotale < minNote) {
+    classementEstime = sortedData[0].classement; // Utiliser le classement minimum si en dessous de la plage
+  } else if (noteTotale > maxNote) {
+    classementEstime = sortedData[sortedData.length - 1].classement; // Utiliser le classement maximum si au-dessus de la plage
+  } else {
+    // Interpolation linéaire si la noteTotale est dans la plage
+    for (let i = 0; i < sortedData.length - 1; i++) {
+      const point1 = sortedData[i];
+      const point2 = sortedData[i + 1];
+      if (point1.note <= noteTotale && noteTotale <= point2.note) {
+        const slope = (point2.classement - point1.classement) / (point2.note - point1.note);
+        classementEstime = point1.classement + slope * (noteTotale - point1.note);
+        break;
+      }
+    }
+  }
+
+  // Créer le marqueur à l'emplacement exact de la noteTotale et du classement estimé
   const marker = {
     label: 'Classement estimé',
-    data: [{ x: pointGraphique.note, y: pointGraphique.classement }],
+    data: [{ x: noteTotale, y: classementEstime }],
     borderColor: 'rgba(0, 123, 255, 1)',
     backgroundColor: 'rgba(0, 123, 255, 1)',
     pointRadius: 5,
     showLine: false,
-    fill: false
+    fill: false,
   };
 
   // Mettre à jour ou ajouter le marqueur au graphique
   const markerIndex = graphiqueEcos.data.datasets.findIndex(dataset => dataset.label === 'Classement estimé');
   if (markerIndex >= 0) {
-    graphiqueEcos.data.datasets[markerIndex].data = [{ x: pointGraphique.note, y: pointGraphique.classement }];
+    graphiqueEcos.data.datasets[markerIndex].data = [{ x: noteTotale, y: classementEstime }];
   } else {
     graphiqueEcos.data.datasets.push(marker);
   }
@@ -197,42 +237,17 @@ function updateMarker(noteEDN, noteECOS) {
   graphiqueEcos.update();
 }
 
-// Initialiser le formulaire avec les matières et villes
-function initForm(matieres, villes) {
-  const matiereSelect = document.getElementById("matiere");
-  const villeSelect = document.getElementById("ville");
-
-  // Ajouter les matières
-  matieres.forEach((matiere) => {
-    const option = document.createElement("option");
-    option.value = matiere;
-    option.textContent = matiere;
-    matiereSelect.appendChild(option);
-  });
-
-  // Ajouter les villes
-  villes.forEach((ville) => {
-    const option = document.createElement("option");
-    option.value = ville;
-    option.textContent = ville;
-    villeSelect.appendChild(option);
-  });
-}
-
 // Fonction pour générer le graphique en fonction de la sélection
 async function generateChart() {
+  isButtonClicked = true; // Marquer que le bouton a été cliqué
   const selectedMatiere = document.getElementById("matiere").value;
   const selectedVille = document.getElementById("ville").value;
 
   const matiereData = await fetchMatiereData();
   const limitesData = await fetchLimitesData();
 
-  // Récupérer les données de la matière sélectionnée
-  const matiereEntry = matiereData.find(
-    (obj) => Object.keys(obj)[0] === selectedMatiere
-  );
+  const matiereEntry = matiereData.find((obj) => Object.keys(obj)[0] === selectedMatiere);
 
-  // Vérification si la matière a été trouvée
   if (!matiereEntry) {
     alert(`Aucune donnée trouvée pour la matière ${selectedMatiere}`);
     return;
@@ -242,50 +257,62 @@ async function generateChart() {
   const notes = matiereValues.map((d) => d.Note);
   const classements = matiereValues.map((d) => d.Classement);
 
-  // Récupérer la limite pour la ville sélectionnée
   const limiteVille = limitesData.find(
     (obj) => obj.Spécialité === selectedMatiere && obj.Ville === selectedVille
   );
   const limite = limiteVille ? limiteVille["Rang limite ajusté"] : null;
 
-  // Générer le graphique avec la limite
   if (limite) {
     createChart(selectedMatiere, notes, classements, limite);
+
+    const noteEDN = parseFloat(document.getElementById('noteEDN').value);
+    const noteECOS = parseFloat(document.getElementById('noteECOS').value);
+    updateMarker(noteEDN, noteECOS);
   } else {
-    alert(
-      `Aucune limite trouvée pour ${selectedMatiere} dans la ville ${selectedVille}`
-    );
+    alert(`Aucune limite trouvée pour ${selectedMatiere} dans la ville ${selectedVille}`);
   }
+}
+
+// Initialiser le formulaire avec les matières et villes
+function initForm(matieres, villes) {
+  const matiereSelect = document.getElementById("matiere");
+  const villeSelect = document.getElementById("ville");
+
+  matieres.forEach((matiere) => {
+    const option = document.createElement("option");
+    option.value = matiere;
+    option.textContent = matiere;
+    matiereSelect.appendChild(option);
+  });
+
+  villes.forEach((ville) => {
+    const option = document.createElement("option");
+    option.value = ville;
+    option.textContent = ville;
+    villeSelect.appendChild(option);
+  });
 }
 
 // Fonction principale pour démarrer l'application
 async function initApp() {
   const limitesData = await fetchLimitesData();
-
-  // Extraire les villes disponibles à partir des limites
   const villes = [...new Set(limitesData.map((d) => d.Ville))];
 
-  // Initialiser le formulaire avec les matières et les villes
   initForm(matieres, villes);
 
-  // Définir une matière et une ville par défaut
-  const defaultMatiere = matieres[0]; // Par exemple, la première matière
-  const defaultVille = villes[0]; // Par exemple, la première ville
+  const defaultMatiere = matieres[0];
+  const defaultVille = villes[0];
 
-  // Sélectionner les valeurs par défaut dans les menus déroulants
   document.getElementById("matiere").value = defaultMatiere;
   document.getElementById("ville").value = defaultVille;
 
-  // Générer le graphique par défaut
-  generateChart();
+  generateChart(); // Initialiser le graphique sans le marqueur
 }
-const btnChart = document.querySelector(".btn_chart");
 
-btnChart.addEventListener("click", () => {
-
-})
 // Démarrer l'application
 initApp();
+
+
 
 
 const { scrollTop, clientHeight } = document.documentElement;
